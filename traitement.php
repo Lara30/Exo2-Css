d1e4edfdb3ec6dced77a7b449180415e374c234e
akoiaka <akoi@akoi.fr>
2017-05-02 14:44:51 +0200

    mardi aprem

--- traitement.php
+++ traitement.php
@@ -10,46 +10,89 @@
 
       <?php
       $nom = $mail = $message ="";
+      $nomErreur = $mailErreur = $messageErreur="";
+
 // on est obligé de créer un paramètre données qui va nous
 // permettre de renvoyer aux valeurs que l'on a besoin
       function securisation($donnees){
         $donnees = trim($donnees);
         $donnees = stripslashes($donnees);
         $donnees = strip_tags($donnees);
+        $donnees = htmlspecialchars($donnees);
         return $donnees;
       }
-      $nom = securisation($_POST["nom"]);
-      $mail = securisation($_POST["mail"]);
-      $message = securisation($_POST["message"]);
-      echo strtolower ("Votre nom est :".$nom. "<br/>");
-      echo strtolower ("Votre mail est :".$mail. "<br/>");
-      echo strtolower ("Votre message est :".$message);
+// empty=vide
+      if (empty($_POST["nom"]))
+      {
+        $nomErreur = "pas rempli";
+      }
+      else
+        {
+        $nom = securisation($_POST["nom"]);
+          if (preg_match("/[^A-Za-z]/", $nom))
+          {
+            $nomErreur = "caractères spéciaux";
+            $nom = "";
+          }
+          elseif (strlen($nom) > 20)
+          {
+            $nomErreur = "Pas plus de 20 caractères";
+            $nom = "";
+          }
+        }
+// la fonction preg_match sert à définir quels champs sont acceptés
+// et d'autres interdits
+      if (empty($_POST["mail"]))
+      {
+        $mailErreur ="Vous n'avez pas rempli votre mail";
+      }
+      else
+        {
+          $mail = securisation($_POST["mail"]);
+            if (filter_var($mail, FILTER_VALIDATE_EMAIL))
+            {
+              $mailErreur = "Format correct";
+            }
+            else
+            {
+              $mailErreur = "Format non adapté";
+              $mail ="";
+            }
+        }
 
+      if (empty($_POST["message"]))
+      {
+        $messageErreur ="pas rempli";
+      }
+      else
+        {
+          $message = securisation($_POST["message"]);
+          if (strlen($message) > 20)
+          {
+            $messageErreur = "trop de caracteres";
+            $message = "";
+          }
+       }
+// strlen=nombre de caractères dans une string
+      echo strtolower ("Votre nom est :".$nom);echo $nomErreur;
+      echo "<br>";
+      echo strtolower ("Votre mail est :".$mail);echo $mailErreur;
+      echo "<br>";
+      echo strtolower ("Votre message est :".$message);echo $messageErreur;
+      echo "<br>";
       ?>
 
-        <!--minuscule ou majuscule et faire la vérif php des caractères spéciaux   -->
-      <!-- <?php
-      // echo $_POST["nom"];
-      ?>
       <!--avec post on cache les infos qui passent dans les url, cependant rien n'empêche
     un utilisateur de mettre de l'html dans notre formulaire donc ce n'est pas assez
   il faut davantage sécuriser les infos que peut noter l'utilisateur  -->
 
-      <?php
-      // echo htmlspecialchars ($_POST["nom"]);
-      ?>
-      <!--htmlspecialchars sert à éviter le script dans les champs ils ne seront
-       pas interprétés
-    la fonction strip_tags permet de supprimer tout code html présent dans une
-    chaine de caractère.
-      <?php
-      // echo $_POST["mail"];
-      ?>
-      <?php
-      // echo $_POST["message"];
-      ?> -->
-<!--il faut prendre les id mis dans le html pour les reprendre dans le
-php et ainsi les renvoyer au php par la valeur qu'on leur a donné  -->
+  <!--htmlspecialchars sert à éviter le script dans les champs ils ne seront
+  pas interprétés
+la fonction strip_tags permet de supprimer tout code html présent dans une
+chaine de caractère.
+
+il faut prendre les id mis dans le html pour les reprendre dans le -->
+<!-- php et ainsi les renvoyer au php par la valeur qu'on leur a donné  -->
 <!--la fonction trim permet d'éviter des espaces dans les champs l'utilisateur
 ne perturbera pas
 le code avec les espaces, ceux-ci n'apparaîtront pas
